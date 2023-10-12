e82310075fca32388c53beb7ddb7ce6a8326cd4d
yasusi1393 <k020c1087@g.neec.ac.jp>
2023-10-12 11:28:04 +0900

    k

--- app/src/main/java/com/example/goodlife3/FragmentCalendar.java
+++ app/src/main/java/com/example/goodlife3/FragmentCalendar.java
@@ -1,4 +1,49 @@
 package com.example.goodlife3;
 
-public class FragmentCalendar {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FragmentCalendar extends AppCompatActivity {
+
+    private TextView titleText;
+    private Button prevButon, nextButton;
+    private CalendarAdapter mCalendarAdapter;
+    private GridView calendarGridView;
+    private View prevButton;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        titleText = findViewById(R.id.titleText);
+        prevButton = findViewById(R.id.prevButton);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarAdapter.prevMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
+            }
+        });
+        nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarAdapter.nextMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
+            }
+        });
+        calendarGridView = findViewById(R.id.calendarGridView);
+        mCalendarAdapter = new CalendarAdapter(this);
+        calendarGridView.setAdapter(mCalendarAdapter);
+        titleText.setText(mCalendarAdapter.getTitle());
+    }
+
 }
--- app/src/main/java/com/example/goodlife3/MainActivity.java
+++ app/src/main/java/com/example/goodlife3/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.goodlife3;
 
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // データを用意
+        String[] names = {
+                "とり", "しか", "きつね", "かば", "ライオン", "パンダ", "ひつじ"
+        };
+        String[] details = {
+                "「とり」の説明文が入ります。", "「しか」の説明文が入ります。",
+                "「きつね」の説明文が入ります。", "「かば」の説明文が入ります。",
+                "「ライオン」の説明文が入ります。", "「パンダ」の説明文が入ります。",
+                "「ひつじ」の説明文が入ります。"
+        };
+        int[] images = {
+                R.drawable.a, R.drawable.b , R.drawable.c, R.drawable.d,
+                R.drawable.e, R.drawable.e, R.drawable.e
+        };
+
+        ArrayList<Map<String, Object>> listData = new ArrayList<>();
+        for (int i=0; i < names.length; i++) {
+            Map<String, Object> item = new HashMap<>();
+            item.put("name", names[i]);
+            item.put("detail", details[i]);
+            item.put("image", images[i]);
+            listData.add(item);
+        }
+
+        // ListViewにデータをセットする
+        ListView list = findViewById(R.id.list);
+        list.setAdapter(new SimpleAdapter(
+                this,
+                listData,
+                R.layout.list_item,
+                new String[] {"name", "detail", "image"},
+                new int[] {R.id.name, R.id.detail, R.id.image}
+        ));
     }
 }
